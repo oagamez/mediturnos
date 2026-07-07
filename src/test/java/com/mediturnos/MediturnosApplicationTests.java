c2eb1a5b52276725e327bc32a6e974d12c141b81
Orlando Agamez <oagamez@unicesar.edu.co>
2026-07-07 10:14:36 -0500

    feat: configuración inicial del proyecto

---
+++ src/test/java/com/mediturnos/MediturnosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediturnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediturnosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
